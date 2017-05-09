e1d6d629c78db700c7172b3ee7ccc3ad5d8d44b9
povilas.pupkys <povilas.pupkys@gmail.com>
2017-05-09 11:08:24 +0300

    15 uzduotis

---
+++ 15_uzduotis/src/Main.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class Main {
+	static ArrayList<String> data = new ArrayList<>();
+	static ArrayList<Integer> dayTemp = new ArrayList<>();
+	static ArrayList<Integer> nightTemp = new ArrayList<>();
+	static int days;
+	static int goodDays = 0;
+	static int badDays = 0;
+	static int honey = 0;
+	static double vid = 0;
+
+	public static void main(String[] args) throws IOException {
+		readFromFile();
+		processData();
+		doTheMagic();
+		writeToFile();
+	}
+
+	public static void readFromFile() throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+		try {
+			String line = br.readLine();
+
+			while (line != null) {
+				data.add(line);
+				line = br.readLine();
+			}
+		} finally {
+			br.close();
+		}
+	}
+
+	public static void processData() {
+		days = Integer.valueOf(data.get(0));
+		for (String i : data) {
+			String[] numbers = i.split(" ");
+			if (numbers[0].equals("1")) {
+				dayTemp.add(Integer.valueOf(numbers[1]));
+				nightTemp.add(Integer.valueOf(numbers[2]));
+			} else if (numbers[0].equals("0")) {
+				badDays++;
+			}
+		}
+	}
+
+	public static void doTheMagic() {
+		for (int i = 0; i < dayTemp.size(); i++) {
+			if ((dayTemp.get(i) >= 21 && dayTemp.get(i) <= 25) && (nightTemp.get(i) >= 14 && nightTemp.get(i) <= 16)) {
+				goodDays++;
+				honey = honey + 3;
+			} else if (dayTemp.get(i) > 25 && nightTemp.get(i) > 16) {
+				honey = honey + 1;
+			} else if (dayTemp.get(i) < 21 && nightTemp.get(i) <= 14) {
+				honey = honey + 2;
+
+			}
+		}
+
+		vid = (double) honey / (double) days;
+	}
+
+	public static void writeToFile() {
+		try {
+			PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
+			writer.println("Per " + days + " dienu bites sunese " + honey + " kg. medaus.");
+			writer.println("Lietingu dienu kiekis: " + badDays);
+			writer.println("Medunesiui palankiu dienu kiekis: " + goodDays);
+			writer.println("Vidutiniskai bites per diena prinese " + vid + " kg. medaus");
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
